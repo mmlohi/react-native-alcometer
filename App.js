7c3d42704bdc0ff6060bb84a216ad0e04512b363
Minna  Lohi <101732406+mmlohi@users.noreply.github.com>
2023-09-08 19:16:58 +0300

    Added colorTheme

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
+
 import React, { useState } from 'react';
 import { ScrollView, Text, TextInput, TouchableOpacity, View, Alert } from 'react-native';
-import Styles from './styles/Styles.js';
+import Styles, { DarkStyle, LightStyle } from './styles/Styles.js';
 import NumericInput from "react-native-numeric-input";
 import { RadioButton, Switch } from 'react-native-paper';
 
@@ -13,8 +13,9 @@ export default function App() {
   const [resultText, setResultText] = useState('');
   const [resultColor, setResultColor] = useState('black');
   const [radioval, setRadioval] = useState("male");
-  const [isOn, setIsOn] = useState(false);
-  const info = isOn ? "Dark Theme" : "Light Theme";
+  const [isDarkStyle, setIsDarkStyle] = useState(false);
+  const info = isDarkStyle ? "Switch to Light Theme" : "Switch to Dark Theme";
+  const currentStyle = isDarkStyle ? DarkStyle : LightStyle;
 
   const calculateAlcoholConsentration = () => {
     if (!weight) {
@@ -43,73 +44,78 @@ export default function App() {
       resultColor = 'green';
     } else if (alcoholConsentration <= 0.5 || alcoholConsentration <= 0.22) {
       resultText = 'You may be impaired. Your blood alcohol concentration is ' + alcoholConsentration.toFixed(2) + '‰.';
-      resultColor = '#ffff00';
+      resultColor = '#FFD700';
     } else {
       resultText = 'Do not drive! Your blood alcohol concentration is ' + alcoholConsentration.toFixed(2) + '‰.';
       resultColor = 'red';
     }
-
     setResultText(resultText);
     setResultColor(resultColor);
   };
 
   return (
-    <ScrollView contentContainerStyle={Styles.container}>
-      <View style={Styles.columnContainer}>
+    <ScrollView contentContainerStyle={currentStyle.container}>
+      <View style={currentStyle.columnContainer}>
+      <Switch style={currentStyle.switch} value={isDarkStyle} color='green'
+          onValueChange={newValue => setIsDarkStyle(newValue)}
+          thumbColor={isDarkStyle ? '#0fa817' : 'red'}
+          trackColor={{ false: 'grey', true: 'yellow' }}
+        />
+        <Text style={currentStyle.info}>{info}</Text>
       </View>
-      <Text style={Styles.header}>Alcometer</Text>
+      <Text style={currentStyle.header}>Alcometer</Text>
       <RadioButton.Group
         onValueChange={newValue => setRadioval(newValue)}
-        value={radioval}>
-        <View style={Styles.radioStyle}>
+        value={radioval}
+      >
+        <View style={currentStyle.radioStyle}>
           <RadioButton
-            labelStyle={Styles.radioButton}
+            labelStyle={currentStyle.radioButton}
             value='male'
             color='green'
           />
-          <Text style={Styles.radioText}>Male</Text>
-
-          <View style={Styles.radioStyle}>
+          <Text style={currentStyle.radioText}>Male</Text>
+          <View style={currentStyle.radioStyle}>
             <RadioButton
-              labelStyle={Styles.radioButton}
+              labelStyle={currentStyle.radioButton}
               value='female'
               color='green'
             />
           </View>
-          <Text style={Styles.radioText}>Female</Text>
+          <Text style={currentStyle.radioText}>Female</Text>
         </View>
       </RadioButton.Group>
-      <Text style={Styles.label}>Weight</Text>
-      <View style={Styles.textInputContainer}>
+      <Text style={currentStyle.label}>Weight</Text>
+      <View style={currentStyle.textInputContainer}>
         <TextInput
-          style={Styles.textInput}
+          style={currentStyle.textInput}
           keyboardType="decimal-pad"
           onChangeText={(text) => setWeight(text)}
-          placeholder="weight (kg)"
+          placeholder="   (kg)"
         />
       </View>
-      <Text style={Styles.label}>Bottles</Text>
-      <View style={Styles.numericInputContainer}>
+      <Text style={currentStyle.label}>Bottles</Text>
+      <View style={currentStyle.numericInputContainer}>
         <NumericInput
-          inputStyle={Styles.numericInput}
+          inputStyle={currentStyle.numericInput}
           onChange={(value) => setBottles(value)}
           minValue={0} // Prevent input of negative values.
         />
       </View>
-      <Text style={Styles.label}>Hours</Text>
-      <View style={Styles.numericInputContainer}>
+      <Text style={currentStyle.label}>Hours</Text>
+      <View style={currentStyle.numericInputContainer}>
         <NumericInput
-          inputStyle={Styles.numericInput}
+          inputStyle={currentStyle.numericInput}
           onChange={(value) => setTime(value)}
           minValue={0} // Prevent input of negative values.
         />
       </View>
       <TouchableOpacity onPress={calculateAlcoholConsentration}>
-        <View style={Styles.submitContainer}>
-          <Text style={Styles.submit}>CALCULATE</Text>
+        <View style={currentStyle.submitContainer}>
+          <Text style={currentStyle.submit}>CALCULATE</Text>
         </View>
       </TouchableOpacity>
-      <Text style={[Styles.resultText, { color: resultColor }]}>{resultText}</Text>
+      <Text style={[currentStyle.resultText, { color: resultColor }]}>{resultText}</Text>
     </ScrollView>
   );
 }
--- styles/Styles.js
+++ styles/Styles.js
@@ -1,40 +1,46 @@
-import { StyleSheet } from "react-native";
-import Constants from "expo-constants";
+import { StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
 
-
-export default StyleSheet.create({
+const LightStyle = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: Constants.statusBarHeight + 5,
-    backgroundColor: "#FFFFFF", 
+    backgroundColor: '#f6f4f4',
     borderWidth: 1,
     borderRadius: 10,
     margin: 10,
     padding: 10,
-    flexDirection: "column",
-    justifyContent: "space-between",
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  },
+  columnContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
   },
   switch: {
-    alignSelf: "flex-end",
-    marginVertical: 5,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  info: {
+    fontSize: 12,
   },
   header: {
-    fontSize: 24,
-    color: "black",
-    fontWeight: "bold",
+    fontSize: 30,
+    color: 'black',
+    fontWeight: 'bold',
     paddingBottom: 10,
-    textAlign: "center",
+    textAlign: 'center',
   },
   label: {
-    color: "black",
-    fontWeight: "bold",
+    color: 'black',
+    fontWeight: 'bold',
     fontSize: 18,
     paddingLeft: 10,
-    textAlign: "center",
+    textAlign: 'center',
   },
-  textInputContainer:{
-    justifyContent: 'center', 
-    alignItems: "center",
+  textInputContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   textInput: {
     width: 80,
@@ -45,53 +51,150 @@ export default StyleSheet.create({
     padding: 10,
     margin: 10,
     marginTop: 5,
-    alignItems: "center",
-    justifyContent: 'center', 
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  submitContainer:{
-    alignItems: 'center', 
+  submitContainer: {
+    alignItems: 'center',
   },
   submit: {
     width: 180,
     borderWidth: 1,
     borderRadius: 10,
-    color: "white",
-    backgroundColor: "green", 
+    color: 'white',
+    backgroundColor: 'green',
     padding: 15,
     margin: 10,
-    textAlign: "center",
+    textAlign: 'center',
     fontSize: 22,
-    fontWeight: "bold",
+    fontWeight: 'bold',
   },
   numericInputContainer: {
     flexDirection: 'row',
-    justifyContent: 'center', // Keskitä vaakasuunnassa
-    alignItems: 'center', // Keskitä pystysuunnassa
-   
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  buttonStyle:
-   { borderColor: "green",
-    borderRadius: 10 },
+  buttonStyle: {
+    borderColor: 'green',
+    borderRadius: 10,
+  },
   numericInput: {
     paddingLeft: 10,
     borderWidth: 5,
-    borderColor: "green", 
-    borderRadius: 5, 
-
+    borderColor: 'green',
+    borderRadius: 5,
   },
   radioStyle: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent:"center"
-    
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  radioText:{
-    fontWeight: "bold",
+  radioText: {
+    fontWeight: 'bold',
     fontSize: 18,
   },
   resultText: {
-    fontSize: 24, 
+    fontSize: 24,
     textAlign: 'center',
-     
-  }
+  },
 });
+
+const DarkStyle = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: Constants.statusBarHeight + 5,
+    backgroundColor: 'grey',
+    borderWidth: 1,
+    borderRadius: 10,
+    margin: 10,
+    padding: 10,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  },
+  columnContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  switch: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  info: {
+    fontSize: 12,
+  },
+  header: {
+    fontSize: 30,
+    color: 'black',
+    fontWeight: 'bold',
+    paddingBottom: 10,
+    textAlign: 'center',
+  },
+  label: {
+    color: 'black',
+    fontWeight: 'bold',
+    fontSize: 18,
+    paddingLeft: 10,
+    textAlign: 'center',
+  },
+  textInputContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textInput: {
+    width: 80,
+    fontSize: 18,
+    paddingLeft: 10,
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
+    margin: 10,
+    marginTop: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  submitContainer: {
+    alignItems: 'center',
+  },
+  submit: {
+    width: 180,
+    borderWidth: 1,
+    borderRadius: 10,
+    color: 'white',
+    backgroundColor: 'green',
+    padding: 15,
+    margin: 10,
+    textAlign: 'center',
+    fontSize: 22,
+    fontWeight: 'bold',
+  },
+  numericInputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonStyle: {
+    borderColor: 'green',
+    borderRadius: 10,
+  },
+  numericInput: {
+    paddingLeft: 10,
+    borderWidth: 5,
+    borderColor: 'green',
+    borderRadius: 5,
+  },
+  radioStyle: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  radioText: {
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+  resultText: {
+    fontSize: 24,
+    textAlign: 'center',
+  },
+});
+
+export { LightStyle, DarkStyle };
